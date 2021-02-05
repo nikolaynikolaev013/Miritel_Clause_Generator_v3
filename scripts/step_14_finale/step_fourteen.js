0b45f9c4c6ee503bf8539e25e2aef2344955f4f6
Nikolay Nikolaev <nikolainikolaev013@gmail.com>
2021-02-05 20:36:16 +0200

    added download button to the last final page(test)

--- scripts/step_14_finale/step_fourteen.js
+++ scripts/step_14_finale/step_fourteen.js
@@ -4,11 +4,29 @@ function stepFourteen(userData){
 
     let title = 'Финализиране';
 
-
-    let descriptionP = `Финал`;
+    let clauseText = document.querySelector('#clausePreview').textContent;
+    let descriptionP = clauseText;
     
+    let downloadButton = document.createElement('button');
+    downloadButton.classList.add('button');
+    downloadButton.id = 'downloadButton';
+    downloadButton.textContent = 'Изтегляне на клаузата';
+
 
     fillPage(title, [
-        descriptionP, 'blank',
+        descriptionP, 'blank', downloadButton.outerHTML
     ]);
+
+
+    downloadButton = document.querySelector('#downloadButton');
+    downloadButton.addEventListener('click', ()=>{
+        let clauseText = document.querySelector('#clausePreview').textContent.trim();
+
+        let blob = new Blob(
+            [clauseText], {
+                type: "text/plain;charset=utf-8"
+            });
+
+        saveAs(blob, 'clause.txt');
+    })
 }
